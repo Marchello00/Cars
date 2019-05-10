507ebd658484ba2834d4eadde9c1775ab12c9f4a
Marchello00 <nagov-mark@mail.ru>
2019-05-10 20:03:15 +0300

    main.cpp added

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "gracer.h"
+#include "TmxLevel.h"
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 1280), "X_x_level_x_X");
+
+    sf::Clock clock;
+    double time;
+
+    sf::Image racerImage;
+    racerImage.loadFromFile("images/bv.png");
+
+    GRacer racer(racerImage, 100, 100);
+
+    TmxLevel level;
+    level.LoadFromFile("maps/map2.tmx");
+    while (window.isOpen()) {
+
+        time = clock.getElapsedTime().asMilliseconds();
+        clock.restart();
+
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed ||
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        level.Draw(window);
+        racer.draw(window);
+
+        window.display();
+    }
+    return 0;
+}
